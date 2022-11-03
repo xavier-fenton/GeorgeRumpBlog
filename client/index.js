800c71d2e45acc084c108762bfa71c4d7cfe58b1
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-03 22:40:20 +1300

    comment out redux

--- client/index.js
+++ client/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/subApp/App'
-import store from './store'
+// import store from './store'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>,
+    <Router>
+      <App />
+    </Router>,
+
     document.getElementById('app')
   )
 })
