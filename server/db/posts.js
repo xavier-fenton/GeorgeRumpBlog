aab364da1c68341dd860894a2c3b19ec35237aab
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-03 21:54:32 +1300

    addPost function

--- server/db/posts.js
+++ server/db/posts.js
@@ -3,7 +3,17 @@ const connection = require('./connection')
 function getPosts(db = connection) {
   return db('posts').select()
 }
+function addPost(post, created_at, db = connection) {
+  return db('posts')
+    .insert({
+      created_at: created_at,
+      id: post.id,
+      post: post.post,
+    })
+    .select()
+}
 
 module.exports = {
   getPosts,
+  addPost,
 }
