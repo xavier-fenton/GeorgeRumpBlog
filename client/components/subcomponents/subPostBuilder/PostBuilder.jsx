3c789735f65fc5778e4655162cf328bb299435f6
Xavier Fenton <xavierfenton99@gmail.com>
2022-11-04 16:12:21 +1300

    add postBuilder comp

---
+++ client/components/subcomponents/subPostBuilder/PostBuilder.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const PostBuilder = () => {
+  const [item, setItems] = useState([
+    {
+      type: 'some-type',
+      id: 'some-id',
+      content: 'somecont',
+    },
+  ])
+
+  return (
+    <div className="posts-styling">
+      {item.map((items) => (
+        <p key={item.id}>{items.type}</p>
+      ))}
+    </div>
+  )
+}
+
+export default PostBuilder
